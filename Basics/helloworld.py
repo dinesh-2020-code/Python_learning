c3eeb1aed91208f89bed1d9792481e1e41631d17
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-03-03 12:17:58 +0530

    First Commit

---
+++ Basics/helloworld.py
@@ -0,0 +1,50 @@
+# print('hello world!')
+
+# print("Python's strings are easy to use.")
+
+# name = input("Please enter your name: ")
+# print("hello" + ' ' + name)
+
+"""
+
+In python all functions return the value.
+
+
+Variables and Types: 
+    When a program's running, everything the program needs ends up stored in the computer's memory. The program
+    code itself will be stored in one area of memory, and the data it works on will also be stored somewhere in 
+    the memory. 
+    
+    A variable is basically just a way to give a name to an area of memory, into which we can place certain values.
+        Variables are created when they are first bound to a value, using =.
+
+    defining a variable means to bind the value to a variable of specific type
+
+
+    Python Data Types
+        -> numeric
+        -> iterator
+        -> sequence (which are also iterators)
+        -> mapping
+        -> file
+        -> class
+        -> exception
+
+
+
+"""
+
+
+# greeting = 'hello'
+# print(greeting)
+# print(type(greeting))
+
+# greeting = 2
+# print(greeting)
+# print(type(greeting))
+
+# name = 'Aman'
+# age = 21
+# print(name + ' is ' + age + " years old")  # error: TypeError: can only concatenate str (not "int") to str
+
+
