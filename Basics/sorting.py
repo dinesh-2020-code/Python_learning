ecb719efc7316c51296c24e93f9cc3f80d4b8fe8
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-03-11 22:51:54 +0530

    Case Insensitive Sorting

--- Basics/sorting.py
+++ Basics/sorting.py
@@ -1,15 +1,32 @@
 
 # sorted() can be used to sort any iterable object
-pangram = "The quick brown fox jumps over the lazy dog"
-letters = sorted(pangram) # Return a new list containing all items from the iterable in ascending order.
-print(letters)
+# pangram = "The quick brown fox jumps over the lazy dog"
+# letters = sorted(pangram) # Return a new list containing all items from the iterable in ascending order.
+# print(letters)
 
-numbers = [2.3, 4.5, 3.1, 1.6]
-sorted_numbers = sorted(numbers)
-print(sorted_numbers)
-print(numbers)
+# numbers = [2.3, 4.5, 3.1, 1.6]
+# sorted_numbers = sorted(numbers)
+# print(sorted_numbers)
+# print(numbers)
 
-another_sorted_numbers = numbers.sort() # Sort in-place and change the original list 'numbers'
-print(numbers)
+# another_sorted_numbers = numbers.sort() # Sort in-place and change the original list 'numbers'
+# print(numbers)
 # print(another_sorted_numbers) # prints 'None' to the console and sort() method doesn't return anything
 
+# Case in-sensitive sorting
+missing_letter = sorted("The quick brown fox jumped over the lazy dog",
+                        key=str.casefold)
+
+print(missing_letter)
+
+
+names = ["Graham",
+         "John",
+         "terry",
+         "eric", 
+         "Terry", 
+         "michael"
+        ]
+
+names.sort(key=str.casefold)
+print(names)
