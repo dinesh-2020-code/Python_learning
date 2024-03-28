e0d4c4da5a8d42cf50bc9b4aef64614d6cdefc8f
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-03-28 21:47:16 +0530

    Demo to hash function

---
+++ Dictionary And Sets/atrocious_hash.py
@@ -0,0 +1,46 @@
+data = [
+    ("orange", "a sweet, orange, citrus fruit"),
+    ("apple", "good for making cider"),
+    ("lemon", "a sour, yellow citrus fruit"),
+    ("grape", "a small, sweet fruit growing in bunches"),
+    ("melon", "sweet and juicy"),
+]
+# print(ord("a"))  # 97
+# print(ord("b"))  # 98
+# print(ord("z"))  # 122
+
+
+def simple_hash(s: str) -> int:
+    """A ridiculously simple hashing function"""
+    basic_hash = ord(s[0])
+    return basic_hash % 10
+
+
+def get(k: str) -> str:
+    """Returns the value for a key, or `None` if the key doesn't exist"""
+    hash_code = simple_hash(k)
+    if values[hash_code]:
+        return values[hash_code]
+    else:
+        return None
+
+
+keys = [""] * 10
+values = keys.copy()
+
+for key, value in data:
+    h = simple_hash(key)
+    # print(key, h)
+    keys[h] = key
+    values[h] = value
+
+print(keys)
+print(values)
+print(get("grape"))
+print(get("lemon"))
+print(get("banana")) # `banana` and `lemon` produces the same hash value
+
+# for key, value in data:
+#     # h = simple_hash(key)
+#     h = hash(key)
+#     print(key, h)
