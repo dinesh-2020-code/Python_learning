8869c721a06e3518b0d8bf9494a106932ce62d02
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-03-31 21:36:17 +0530

    Removing Duplicates using set

--- Dictionary And Sets/modifying_sets.py
+++ Dictionary And Sets/modifying_sets.py
@@ -5,7 +5,19 @@
 numbers = set() # <class 'set'>
 # Adding elements to set
 # numbers.add(1)
-while (len(numbers) < 4):
-    next_value = int(input("please enter you next value: "))
-    numbers.add(next_value)
-print(numbers)
+# while (len(numbers) < 4):
+#     next_value = int(input("please enter you next value: "))
+#     numbers.add(next_value)
+# print(numbers)
+
+data = ["blue", "red", "blue", "green", "red", "blue", "white"]
+
+# Create a set from the list, to remove duplicates.
+# unique_data = set(data)
+unique_data = sorted(set(data))  # produces a list of the data in ascending order
+print(unique_data)
+
+# Create a list of unique colours, keeping the order they appear
+# dict.fromkeys() method: This method create a new dictionary with keys from iterable and values set to value.
+unique_data = list(dict.fromkeys(data))  # insertion order is preserved here.
+print(unique_data)
