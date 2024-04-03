bc865daa10d28d4cf72fe80393cfc46cb858da55
Dinesh <thedineshkumar2002@gmail.com>
2024-04-03 08:05:43 -0700

    Subsets and supersets

--- Dictionary And Sets/subsets.py
+++ Dictionary And Sets/subsets.py
@@ -20,3 +20,16 @@ birds = {'Robin', 'Swallow', 'Wren'}
 print(f'Birds is a subset of animals: {birds.issubset(animals)}')   # birds <= animals
 print(f'animals is a superset of birds: {animals.issuperset(birds)}')
 
+print(f'Birds is a superset of animals: {birds.issuperset(animals)}')
+
+print('*' * 80)
+
+garden_birds = {'Swallow', 'Wren', 'Robin'}
+print(garden_birds == birds)  # True
+print(garden_birds <= birds)  # True
+print(garden_birds < birds )  # check for proper subset returns False
+
+print('*' * 80)
+
+more_birds = {'Wren', 'Budgie', 'Swallow'}
+print(garden_birds >= more_birds)  # False
