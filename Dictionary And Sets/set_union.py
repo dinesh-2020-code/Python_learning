c943582924b178538e64fcea2df085f9db50edd1
Dinesh <thedineshkumar2002@gmail.com>
2024-04-02 19:16:17 -0700

    set update() method

--- Dictionary And Sets/set_union.py
+++ Dictionary And Sets/set_union.py
@@ -14,9 +14,12 @@ from prescription_data import adverse_interactions
 
 meds_to_watch = set()
 
-for interaction in adverse_interactions:
-    # meds_to_watch = meds_to_watch.union(interaction)
-    # meds_to_watch = meds_to_watch | interaction
-    # meds_to_watch.update(interaction)  # Updates the existing set
-    meds_to_watch |= interaction
+# for interaction in adverse_interactions:
+#     # meds_to_watch = meds_to_watch.union(interaction)
+#     # meds_to_watch = meds_to_watch | interaction
+#     # meds_to_watch.update(interaction)  # Updates the existing set
+#     meds_to_watch |= interaction
+
+meds_to_watch.update(*adverse_interactions)
 print(sorted(meds_to_watch))
+print(*sorted(meds_to_watch), sep="\n")
