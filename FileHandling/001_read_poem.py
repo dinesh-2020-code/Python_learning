a5093e84a85a934a817c2608fc8a7f7624b6c61d
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-06 17:49:32 +0530

    Reading from file

--- FileHandling/001_read_poem.py
+++ FileHandling/001_read_poem.py
@@ -1,3 +1,14 @@
+"""
+    Whitespace: 
+        Whitespace is any character that represents horizontal or vertical spacing.
+        Whitespace characters aren't visible as characters, but they do space things out.
+        Ex: space, tab, newline
+    str.strip(): Return a copy of the string with leading and trailing whitespace removed
+"""
 jabber = open('jabberwocky.txt', 'r')
 
+for line in jabber:
+    # print(line, end='')  # to removing '\n' char from each line
+    print(line.strip())  # strip off the '\n' char from end of line
 
+jabber.close()
