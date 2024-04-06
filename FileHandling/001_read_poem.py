984ad0614f43cbce46ee4ea1e1bad8dd7490fbbb
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-06 18:15:16 +0530

    using  statement in python

--- FileHandling/001_read_poem.py
+++ FileHandling/001_read_poem.py
@@ -4,11 +4,28 @@
         Whitespace characters aren't visible as characters, but they do space things out.
         Ex: space, tab, newline
     str.strip(): Return a copy of the string with leading and trailing whitespace removed
+
+    File handle:
+        File handles(or file descriptors, on Mac and Linux) are used by the Operating system,
+        to keep track of the files that have been opened.
+
+        They are system-wide, which means the available file handles are shared by every process
+        running on your computer.
+
+        Another consequence of failing to close a file, when writing data to it, is that data
+        might be lost. 
+        Neither of these situations are good, so it's very important to close every file that you open.
+
+        Python closes the file automatically if we open the file using 'with' statement.
 """
-jabber = open('jabberwocky.txt', 'r')
+# jabber = open('jabberwocky.txt', 'r')
 
-for line in jabber:
-    # print(line, end='')  # to removing '\n' char from each line
-    print(line.strip())  # strip off the '\n' char from end of line
+# for line in jabber:
+#     # print(line, end='')  # to removing '\n' char from each line
+#     print(line.strip())  # strip off the '\n' char from end of line
 
-jabber.close()
+# jabber.close()
+
+with open('jabberwocky.txt', 'r') as jabber:
+    for line in jabber:
+        print(line.strip())
