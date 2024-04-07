96ccd247241018e994395346138c4ccb1cbe2328
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-07 22:28:39 +0530

    File modes and Buffer

--- FileHandling/004_save_flowers.py
+++ FileHandling/004_save_flowers.py
@@ -49,6 +49,31 @@ data = [
         Also no conversion is performed. If you tell `write` to write an integer, that's what it will try to send to the
         file. If the file is opened in text mode (the default), you'll get an error if you try to write numerical
         values to it.
+        
+        File Modes: 
+            'r': open for reading (default)
+            'w': open for writing, truncating the file first
+            'x': open for exclusive creation, failing if the file already exists
+            'a': open for writing, appending to the end of the file it it exists
+            'b': binary mode
+            't': text mode (default)
+            '+': open for updating (reading and writing)
+        
+        Buffer (or cache)
+            -> Because disk drives are slow - compared to computer memory, they're very slow - data is read in large chunks
+                into an area of memory called a "buffer".
+            -> That makes the data available when you want to read more data, without having to wait for the disk drive to 
+                spin around again. 
+            -> Of course, if you're using modern SSD drives, they don't spin. But they're still a lot slower that your
+                computer's memory. 
+            -> By reading larger chunks from the drive, the next data that you'll probably want is made available faster. 
+                A disk buffer is also called a `disk cache`.
+            -> When writing data to disk, the data is put into a buffer first. 
+                That allows your program to move on, without waiting for the actual transfer to complete.
+                The write to disk can happen in the background, while your code gets on with other things.
+                That's another reason why it's very important to always close a file, when writing. 
+                If you fail to close the file, and your program terminates, data in teh cache might not be written to
+                the disk - resulting in data loss.
 '''
 # plants_filename = "flowers_write.txt"
 # with open(plants_filename, 'w') as plants:
