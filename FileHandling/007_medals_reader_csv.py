d36bbc93235b6f4f33e7382a39d243cf0efc2283
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-09 09:25:58 +0530

    Added Olympic Medals_2020 csv file

---
+++ FileHandling/007_medals_reader_csv.py
@@ -0,0 +1,15 @@
+"""
+    CSV (Comma Separated Values)
+        -> The basic idea is that each value on a row is separated form the preceding and following values, with a comma
+        -> CSV files are commonly used to export data from one spreadsheet program, to import it into another
+        spreadsheet, or a database.
+        -> Surprisingly, although CSV files have been used for nearly 50 years, there's no standard definition of CSV.
+        -> The closest thing we've found to a standard, is the `Internet Engineering Task Force (IETF) RFC 4180`
+        -> RFC is a `Request for Comments`, and these documents form the basis for standards on the internet.
+        But RFC 4180 doesn't define a standard for CSV files.
+
+        -> A CSV file contains rows of data, with each row having the same number of "fields". Each field is separated
+        by some character. That can be comma, but you can also use tabs or spaces, or any other character that's
+        appropriate.
+
+"""
