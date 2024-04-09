65387a8610c55b0306ad7a3809ee1fcb99b32e86
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-09 09:10:17 +0530

    Fixed code issue

--- FileHandling/006_global_temps.py
+++ FileHandling/006_global_temps.py
@@ -20,7 +20,7 @@ json_data_source = 'https://www.ncei.noaa.gov/access/monitoring/climate-at-a-gla
 # with open(json_data_source, encoding='utf-8') as data:
 with urllib.request.urlopen(json_data_source) as json_stream:
     data = json_stream.read().decode('utf-8')
-    anomalies = json.load(data)
+    anomalies = json.loads(data)  # Deserializing string 'data' to python object
 
 print(anomalies['description'])
 
