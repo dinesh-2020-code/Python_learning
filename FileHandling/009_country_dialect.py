f7e5313f4479427cdb5e9aebb78a2d4073d7c335
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-09 22:09:57 +0530

    Dialect printing

--- FileHandling/009_country_dialect.py
+++ FileHandling/009_country_dialect.py
@@ -8,12 +8,25 @@ with open(input_filename, encoding='utf-8', newline='') as countries_data:
         sample += countries_data.readline()
     sample = countries_data.read()
     country_dialect = csv.Sniffer().sniff(sample)
+    country_dialect.skipinitialspace = True
     countries_data.seek(0)  # setting the file ptr to beg of file as in line 6, read() method place it to the end.
     # country_reader = csv.reader(countries_data, delimiter='|')
     country_reader = csv.reader(countries_data, dialect=country_dialect)
     for row in country_reader:
         print(row)
 
+print("*" * 80)
+attributes = ['delimiter',
+              'doublequote',
+              'escapechar',
+              'lineterminator',
+              'quotechar',
+              'quoting',
+              'skipinitialspace'
+              ]
+
+for attribute in attributes:
+    print(f'{attribute}: {repr(getattr(country_dialect, attribute))}')
 
 """
     seek():
