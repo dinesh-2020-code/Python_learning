38e091c4ecd747350d4c19973cbf0c41e3021c76
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-10 21:14:31 +0530

    Field Names with DictReader

--- FileHandling/012_country_challenge.py
+++ FileHandling/012_country_challenge.py
@@ -2,13 +2,20 @@ import csv
 
 country_filename = 'country_info.txt'
 
+dialect = csv.excel
+dialect.delimiter = '|'
+
 countries = {}
 
 with open(country_filename, encoding='utf-8', newline='') as country_file:
-    reader = csv.DictReader(country_file, delimiter='|')
+    # Get the header from teh first line of the file
+    headings = country_file.readline().strip('\n').split(dialect.delimiter)
+    for index, heading in enumerate(headings):
+        headings[index] = heading.casefold()
+    reader = csv.DictReader(country_file, dialect=dialect, fieldnames=headings)
     for row in reader:
-        countries[row['Country'].casefold()] = row
-        countries[row['CC'].casefold()] = row
+        countries[row['country'].casefold()] = row
+        countries[row['cc'].casefold()] = row
 
 # print(countries)
 
@@ -18,7 +25,7 @@ while True:
     if country_key in countries:
         country_data = countries[country_key]
         print(country_data)
-        country_capital = country_data['Capital']
+        country_capital = country_data['capital']
         print(f"The capital of '{country_name}' is '{country_capital}'")
     elif country_name == 'quit':
         break
