57d3cf8e215b5311e9c0970e29e64747627389a2
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-19 19:24:00 +0530

    More on byte and bytes array object

--- FileHandling/017_bytes_example.py
+++ FileHandling/017_bytes_example.py
@@ -27,10 +27,44 @@
                 Python 3                                              Python 2
                 'This is a unicode string (type str)'   u'This is a unicode string in Python 2'
                 b'This is a byte array'                 'This is an array of bytes in Python 2. Its type is str'
+
+        -> byte-like objects:
+            bytes and bytearray objects are often used to store Unicode data, so Python provides a way to create
+            a bytes object using familiar string syntax.
+
+            The main differences are that we prefix the string with the letter `b`, and we can use hexadecimal
+            values for individual bytes.        
+
+            \x escape sequence tells python that what follows will be a hexadecimal value.
+            Note that we don't have to use hexadecimal. We can use ASCII characters, and Python will replace
+            the character with the ASCII code for that char.
+            ASCII code for lowercase letter 'r' is 114 in decimal, or 72 in hex.
+
+        -> Text files are a special case
+
+            There's really no such things as a text file, as far as the computer is concerned. 
+            We can read the text in a file, because our editor decodes it and presents the appropriate
+            characters on the screen.
+
+            Python performs that decoding for us, if we specify text mode, when we open the file. 
+            Python also splits the bytes up at each newline character.
+
+        Unicode is just binary data:
+            So all files really contain binary data, but text files are treated differently when specify
+            text mode.
+
+            If you wanted to open a text file in binary mode, you can. You'd need to split the data up at
+            the newlines, and decode the binary data yourself, but it would work.
 '''
 
 # equation = bytes((207, 128, 114, 194, 178))
-equation = b'\xcf\x80r\xc2\xb2'
+equation = b'\xcf\x80r\xc2\xb2'  # bytes object (array of bytes - an array of 8 bit numbers)
 print(equation)
 print(type(equation))
 print(len(equation))
+
+for b in equation:
+    print(b, end=' ')
+print()
+
+print(equation.decode('utf-8'))
