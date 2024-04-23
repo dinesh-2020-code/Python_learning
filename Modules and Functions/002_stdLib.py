4609ffe5f53fe5a442a980998f6d6e1ba3180673
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-23 22:16:37 +0530

    python shelve module

--- Modules and Functions/002_stdLib.py
+++ Modules and Functions/002_stdLib.py
@@ -2,3 +2,15 @@
 # # print(dir(__builtins__))
 # for m in dir(__builtins__):
 #     print(m)
+
+import shelve
+
+# print(dir())
+# print()
+# print(dir(shelve) )
+
+# for obj in dir(shelve.Shelf):
+#     if obj[0] != '_':
+#         print(obj)
+
+help(shelve)
