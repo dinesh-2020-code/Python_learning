134ac6d7beaa8c6bd803984258d597feb62d4c98
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-23 22:07:58 +0530

    Module imports

--- Modules and Functions/001_turtleMod.py
+++ Modules and Functions/001_turtleMod.py
@@ -1,8 +1,18 @@
-import turtle
-import time
+# import turtle
+# from turtle import forward, right, done
+from turtle import *  # though not recommended
+# import time
 
-turtle.forward(150)
-turtle.right(250)
-turtle.forward(150)
+# turtle.forward(150)
+# turtle.right(250)
+# turtle.forward(150)
 
-time.sleep(5)
+# following statements for import done using 'from' statement
+forward(150)
+right(250)
+circle(75)
+forward(150)
+done()
+
+# turtle.done()  # the turtle window will not close by itself.
+# time.sleep(5)
