65b7b8672c888215a16326a5b7c56c8801d0272b
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-29 09:00:21 +0530

    time.localtime() function

--- Modules and Functions/004_datecalc.py
+++ Modules and Functions/004_datecalc.py
@@ -2,7 +2,12 @@ import time
 
 print(time.gmtime(0))
 
-print(time.localtime())
+# print(time.localtime(time.time()))
 
-print(time.time()) # prints the no. of seconds passed since epoch (Jan 1, 1970)
+# print(time.time()) # prints the no. of seconds passed since epoch (Jan 1, 1970)
 
+time_here = time.localtime() # returns the named tuple
+print(time_here)
+print("Year:", time_here[0], time_here.tm_year)
+print("Month:", time_here[1], time_here.tm_mon)
+print("Day:", time_here[2], time_here.tm_mday)
