a93864ce81e805dfdac56f9bd16d5b64bc030daa
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-29 09:13:49 +0530

    time.strftime() method

--- Modules and Functions/004_datecalc.py
+++ Modules and Functions/004_datecalc.py
@@ -1,13 +1,29 @@
 import time
+from time import time as my_timer
+import random
 
-print(time.gmtime(0))
+input("Press enter to start")
+
+wait_time = random.randint(1, 6)
+time.sleep(wait_time)
+start_time = my_timer()
+input("Press enter to stop")
+
+end_time = my_timer()
+
+print("Started at " + time.strftime("%X", time.localtime(start_time)))
+print("Ended at " + time.strftime("%X", time.localtime(end_time)))
+
+print("Your reaction time was {} seconds".format(end_time - start_time))
+
+# print(time.gmtime(0))
 
 # print(time.localtime(time.time()))
 
 # print(time.time()) # prints the no. of seconds passed since epoch (Jan 1, 1970)
 
-time_here = time.localtime() # returns the named tuple
-print(time_here)
-print("Year:", time_here[0], time_here.tm_year)
-print("Month:", time_here[1], time_here.tm_mon)
-print("Day:", time_here[2], time_here.tm_mday)
+# time_here = time.localtime() # returns the named tuple
+# print(time_here)
+# print("Year:", time_here[0], time_here.tm_year)
+# print("Month:", time_here[1], time_here.tm_mon)
+# print("Day:", time_here[2], time_here.tm_mday)
