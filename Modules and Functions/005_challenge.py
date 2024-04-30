9572db8780f293d909f165f9db08ef1da7372d63
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-30 21:45:26 +0530

    Timezones

---
+++ Modules and Functions/005_challenge.py
@@ -0,0 +1,13 @@
+# Write a small program to display information on the 
+# four clocks whose functions we have just looked at:
+# i.e., time(), perf_counter(), monotonic() and process_time()
+
+# Use the doc for the get_clock_info() function to
+# work out how to call it for each of the clocks.
+
+import time
+
+print(time.get_clock_info('monotonic'))
+print(time.get_clock_info('perf_counter'))
+print(time.get_clock_info('process_time'))
+print(time.get_clock_info('time'))
