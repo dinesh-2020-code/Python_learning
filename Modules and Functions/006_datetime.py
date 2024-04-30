bf4b2b62bdcb2def0f8b4cf76efe1eee893f193b
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-30 21:53:42 +0530

    datetime module

--- Modules and Functions/006_datetime.py
+++ Modules and Functions/006_datetime.py
@@ -1,13 +1,18 @@
-import time
+# import time
 
-print("The epoch on this system starts at: " + time.strftime("%c", time.gmtime(0)))
+# print("The epoch on this system starts at: " + time.strftime("%c", time.gmtime(0)))
 
-print("The current timezone is {0} with an offset of {1}".format(time.tzname[0], time.timezone))
+# print("The current timezone is {0} with an offset of {1}".format(time.tzname[0], time.timezone))
 
-if time.daylight != 0:
-    print("\tDaylight Saving time is in effect for this location")
-    print("\tThe DST timezone is " + time.timezone[1])
+# if time.daylight != 0:
+#     print("\tDaylight Saving time is in effect for this location")
+#     print("\tThe DST timezone is " + time.timezone[1])
 
-print("Local time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-print("UTC time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
+# print("Local time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+# print("UTC time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
 
+import datetime
+
+print(datetime.datetime.today())
+print(datetime.datetime.now())
+print(datetime.datetime.utcnow())
