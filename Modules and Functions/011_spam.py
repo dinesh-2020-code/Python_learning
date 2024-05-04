5c005da974d34220ba7ea8f9a7db69e9a023c5ee
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-05-04 21:34:48 +0530

    Nested functions in python and scope

--- Modules and Functions/011_spam.py
+++ Modules and Functions/011_spam.py
@@ -3,22 +3,24 @@ def spam1():
     def spam2():
 
         def spam3():
-            z = " even more spam"
+            z = " even"
+            z += y
             print("in spam3, locals are {}".format(locals()))
             return z
         
-        y = " more spam"
-        y += spam3()
+        y = " more " + x  # y must exist before spam3 is called
+        y += spam3()      # do not combine these assignments
         print("in spam2, locals are {}".format(locals()))
         return y
     
-    x = "spam"
-    x += spam2()
+    x = "spam" # x must exist before spam2() is called
+    x += spam2()  # do not combine these assignments
     print("in spam1, locals are {}".format(locals()))
     return x
 
 print(spam1())
-
+print(locals())
+print(globals())
 '''
     -> Variables defined in the inner scope are not accessible to the outer scope, but vice-versa is true.
     i.e., inner scope has access to the variables defined in the outer scope.
