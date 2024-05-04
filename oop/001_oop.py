8ef6ec78ff69f8d31bf15da7709f833253d3025c
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-05-04 21:50:37 +0530

    Defining class in python

---
+++ oop/001_oop.py
@@ -0,0 +1,20 @@
+class Kettle(object):
+
+    def __init__(self, make, price) -> None:
+        self.make = make
+        self.price = price
+        self.on = False
+
+
+kenwood = Kettle("Kenwood", 8.99)
+print(kenwood.make)
+print(kenwood.price)
+
+kenwood.price = 12.75
+print(kenwood.price)
+
+hamilton = Kettle("Hamilton", 14.55)
+
+print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+
+# data attributes: variables when bound to the class instance
