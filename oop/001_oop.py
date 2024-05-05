c824492ddc640a2c9b25074c9f8ab66d6b9a060b
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-05-05 16:10:54 +0530

    Working with classes

--- oop/001_oop.py
+++ oop/001_oop.py
@@ -4,6 +4,9 @@ class Kettle(object):
         self.make = make
         self.price = price
         self.on = False
+    
+    def switch_on(self):
+        self.on = True
 
 
 kenwood = Kettle("Kenwood", 8.99)
@@ -17,4 +20,29 @@ hamilton = Kettle("Hamilton", 14.55)
 
 print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
 
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+print(hamilton.on)
+hamilton.switch_on()
+print(hamilton.on)
+
+print(kenwood.on)
+Kettle.switch_on(kenwood)  # the instance has to be passed when calling the method using class name, the instance would be passed onto 'self'
+print(kenwood.on)
 # data attributes: variables when bound to the class instance
+
+print ("*" * 80)
+
+kenwood.power = 1.5
+print(kenwood.power)
+
+# print(hamilton.power)  # AttributeError: 'Kettle' object has no attribute 'power'
+"""
+Class: template for creating objects. All objects created using the same class will have the same characteristics
+Object: an instance of a class.
+Instantiate: Create n instance of a class
+Method: a function defined in a class.
+Attribute: a variable bound to an instance of class
+self: it's the reference to the instance of the class
+constructor: a special method that is executed when an instance of a class is created. __init__() here. 
+"""
