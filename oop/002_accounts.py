1809500208c456cc81bc1f0d046bb153332a0afd
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-05-06 21:34:36 +0530

    Added transaction logs to our Account class

--- oop/002_accounts.py
+++ oop/002_accounts.py
@@ -1,25 +1,44 @@
+import datetime
+import pytz
+
 class Account:
     """Simple Account class with balance"""
+
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.utcnow()
+        return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.transaction_list = []
         print("Account created for " + self.name)
     
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
+            self.transaction_list.append((Account._current_time(), amount))
             self.show_balance()
     
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
             self.balance -= amount
+            self.transaction_list.append((Account._current_time(), -amount))
         else:
             print("Amount to withdraw must be greater than 0 and no more than your account balance")
         self.show_balance()
     
     def show_balance(self):
         print("Balance is {}".format(self.balance))
+
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                tran_type = "deposition"
+            else:
+                tran_type = "withdrawal"
+            print("{:6} {} on {} (local time was {})".format(amount, tran_type, date, date.astimezone()))
 
 
 if __name__ == "__main__":
@@ -31,4 +50,5 @@ if __name__ == "__main__":
     tim.withdraw(400)
     # tim.show_balance()
     tim.withdraw(2000)
+    tim.show_transactions()
     # tim.show_balance()
