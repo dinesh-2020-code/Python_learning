57d1a0f979ea02cda609b6c13cc767fe98bd4df8
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-05-07 19:10:12 +0530

    Account class

--- oop/002_accounts.py
+++ oop/002_accounts.py
@@ -1,6 +1,7 @@
 import datetime
 import pytz
 
+
 class Account:
     """Simple Account class with balance"""
 
@@ -10,34 +11,37 @@ class Account:
         return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.transaction_list = []
-        print("Account created for " + self.name)
+        self._name = name
+        self._balance = balance
+        self._transaction_list = []
+        self._transaction_list.append((Account._current_time(), balance))
+        print("Account created for " + self._name)
+        self.show_balance()
     
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
-            self.transaction_list.append((Account._current_time(), amount))
+            self._balance += amount
+            self._transaction_list.append((Account._current_time(), amount))
             self.show_balance()
     
     def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            self.transaction_list.append((Account._current_time(), -amount))
+        if 0 < amount <= self._balance:
+            self._balance -= amount
+            self._transaction_list.append((Account._current_time(), -amount))
         else:
             print("Amount to withdraw must be greater than 0 and no more than your account balance")
         self.show_balance()
     
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
+        print("Balance is {}".format(self._balance))
 
     def show_transactions(self):
-        for date, amount in self.transaction_list:
-            if amount > 0:
+        for date, amount in self._transaction_list:
+            if amount >= 0:
                 tran_type = "deposition"
             else:
                 tran_type = "withdrawal"
+                amount *= -1
             print("{:6} {} on {} (local time was {})".format(amount, tran_type, date, date.astimezone()))
 
 
@@ -52,3 +56,8 @@ if __name__ == "__main__":
     tim.withdraw(2000)
     tim.show_transactions()
     # tim.show_balance()
+
+    stephen = Account("Stephen", 800)
+    stephen.deposit(100)
+    stephen.withdraw(200)
+    stephen.show_transactions()
