b8939f064ab7de9a25ae7582127b43a4ac7e1df2
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-05-20 08:45:35 +0530

    Employee Class

---
+++ oop/011_employee.py
@@ -0,0 +1,35 @@
+# Challenge: Employee class
+# Static and instance variable
+
+class Employee:
+    """Class contains employee details"""
+
+    employee_count = 101
+
+    def __init__(self, emp_name, emp_desg, emp_sal):
+        self.e_name = emp_name
+        self.e_sal = emp_sal
+        self.e_desg = emp_desg
+        self.e_id = "e" + str(Employee.employee_count)
+        Employee.employee_count += 1
+
+    def __str__(self):
+        """Prints the string representation of object"""
+        return f"\tEmployee Details:\nID: {self.e_id}\nName: {self.e_name}\nDesg.: {self.e_desg}\nSalary: {self.e_sal}"
+
+    @classmethod
+    def total_employees(cls):
+        """Calculates the employee count."""
+        return Employee.employee_count - 101
+
+
+e1 = Employee("John", "Engineering Manager", 100000)
+print(e1)
+print(e1.total_employees())
+
+# creating another employee
+e2 = Employee("Julius", "Team Lead", 1222230)
+print(e2)
+
+print(Employee.total_employees())
+
