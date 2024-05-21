12d1a380671bf611f51ab4a192ba58b3ff0e77f9
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-05-21 09:01:11 +0530

    Handling exceptions

--- Exception Handling/001_exception.py
+++ Exception Handling/001_exception.py
@@ -11,6 +11,15 @@ else:
     pass
 finally:
     pass
+    
+
+try:
+    pass
+    
+except (IndexError, ValueError):  # handling multiple exceptions in single `except` block
+    pass
+    
+
 """
 
 # Index Error
@@ -20,7 +29,12 @@ try:
     index = int(input("Enter index: "))
     print(lst[index])
     print("End of try block")  # this statement will not be executed if exception is raised in line 21
+except IndexError as e:
+    print("Invalid index\n " , e)
+    print(type(e))   # <class 'IndexError'>
+except ValueError as e:
+    print("Invalid type of index\n " , e)
 except:
-    print("Invalid index")
+    print("Some other exception")
 
 print("Terminated Gracefully...")
