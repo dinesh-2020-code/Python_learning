d71fd7cf43f129fb4063202a8cca236dc1e7f380
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-05-21 20:06:51 +0530

    try-except-else clause

--- Exception Handling/002_why_try_except.py
+++ Exception Handling/002_why_try_except.py
@@ -4,7 +4,7 @@ def div(a, b):
         c = a // b
         return c
     else:
-        raise ZeroDivisionError
+        raise ZeroDivisionError("Division by zero:- not possible!!!")
     
 
 a = int(input("Enter first number: "))
@@ -12,6 +12,10 @@ b = int(input("Enter second number: "))
 
 try:
     c = div(a, b)
+    print("Try block executed successfully")
+except ZeroDivisionError as err:
+    print("in exception block:\n" , err)
+else:
     print(f"{a} // {b} is {c}")
-except:
-    print("Zero Division error")
+
+print("Outside try-except-else block")
