c4631e7ae9ffd7d35578d96cb5d65e718d664e90
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-05-22 09:06:33 +0530

    nested try except block

--- Exception Handling/004_nested_try_except.py
+++ Exception Handling/004_nested_try_except.py
@@ -1,17 +1,27 @@
 # Exceptions can be handled by multiple `except` blocks for single try block
 # Nesting of try_except is not readable.
 
+# try:
+#     a = int(input("Enter first number: "))
+#     try:
+#         b = int(input("Enter second number: "))
+#         try:
+#             c = a // b
+#             print(c)
+#         except ZeroDivisionError as e:
+#             print(e)
+#     except ValueError:
+#         print("Value Error from inner")
+
+# except ValueError:
+#     print("Value Error from outer")
+
 try:
     a = int(input("Enter first number: "))
-    try:
-        b = int(input("Enter second number: "))
-        try:
-            c = a // b
-            print(c)
-        except ZeroDivisionError as e:
-            print(e)
-    except ValueError:
-        print("Value Error from inner")
-
-except ValueError:
-    print("Value Error from outer")
+    b = int(input("Enter second number: "))
+    c = a // b
+    print(c)
+except ZeroDivisionError as e:
+    print(e)
+except ValueError as e:
+    print(e)
